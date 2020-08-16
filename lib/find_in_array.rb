280f245ce097196124e95e7bec70055654d0efbd
Victor Romero <romeroa.victor@yahoo.com>
2020-08-16 19:36:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   counter = 0
   while (counter < array.length) do 
-    if array.include?(value_to_find) do 
+    if array[counter] == value_to_find do 
       puts value_to_find
     else 
      return nil
