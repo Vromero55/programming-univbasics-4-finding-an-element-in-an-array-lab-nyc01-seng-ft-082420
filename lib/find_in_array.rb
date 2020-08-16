c90aec8db11ee8c8249e193cbf2c1f7a518e32fc
Victor Romero <romeroa.victor@yahoo.com>
2020-08-16 19:53:02 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,8 +3,8 @@ def find_element_index(array, value_to_find)
   while (counter < array.length) do 
     if array[counter] == value_to_find
       return array.index(value_to_find)
-    else 
-     return "blue"
+    else do
+     return 
       
 end
 counter = counter + 1
